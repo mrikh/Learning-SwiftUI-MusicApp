caf6500497f95104376c3d395dcefe3ed183bfd9
Mayank Rikh <mayankrikh@gmail.com>
2021-02-22 19:24:46 +0000

    Initial Commit

---
+++ MrMusic/MrMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MrMusicApp.swift
+//  MrMusic
+//
+//  Created by Mayank Rikh on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MrMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
